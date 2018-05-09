d9ee18d25b5ee138d656b3f6dcfd2789cad12c74
Jed <jed@m6700.local>
2018-05-09 14:44:46 -0400

    jed@m6700: Wed May  9 18:44:46 UTC 2018

--- sprintcommit.sh
+++ sprintcommit.sh
@@ -6,12 +6,22 @@ COMMIT_TIME=${1:-300}
 while [ 1 ];
 do
 # Fetch everything
+echo
+echo --- Sleeping until `date -d "+${COMMIT_TIME} second"` ---
+echo 
 git fetch --all --tags --prune --verbose
 
 # A commit.
-git commit --all --message "`whoami`@`hostname`: `date --universal`"
+COMMIT_MSG="`whoami`@`hostname`: `date --universal`"
+echo
+echo --- Committing: ${COMMIT_MSG}  ---
+echo 
+git commit --all --message "${COMMIT_MSG}"
 
 # Push
+echo
+echo --- Pushing ---
+echo 
 git push --all --verbose
 
 # Sleep for 15 minutes
