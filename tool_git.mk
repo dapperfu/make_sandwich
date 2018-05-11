b43e6d1dc5989de93e629ad6a421f95a7f91e78d
Jed <jed@m6700.local>
2018-05-11 16:16:41 -0400

    Added target to list latest commits from all heads, tags and remote branches

--- tool_git.mk
+++ tool_git.mk
@@ -4,12 +4,21 @@ commit:
 	git fetch --all --verbose
 	git commit --all --long
 	git push --all
-	
+
+##
+.PHONY: git.heads
+git.heads:
+	@echo --- Head Commits ---
+	@git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
+	@echo --- Tag Commits ---
+	@git for-each-ref --sort=committerdate refs/tags/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
+	@echo --- Remote Commits ---
+	@git for-each-ref --sort=committerdate refs/remotes/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
+
 ## Development Sprint
 .PHONY: sprint
 sprint:
 	$(shell $(realpath ${MK_DIR}/.mk_inc/sprintcommit.sh))
-
 ## Reset
 .PHONY: reset
 reset:
