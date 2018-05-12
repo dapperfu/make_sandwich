0750dc19ab36cb83a46cf9c200a7ad65fe59e891
jed <jed@m6700-LINUX>
2018-05-11 21:31:06 -0400

    jed@m6700: Sat May 12 01:31:06 UTC 2018

--- tool_git.mk
+++ tool_git.mk
@@ -80,7 +80,8 @@ git.sprint:
 .PHONY: git.sprintcommit
 git.sprintcommit:
 	-${SANDWICH_DIR}/sprintcommit.sh ${COMMIT_TIME}
+
 	
 .PHONY: git.xxx
 git.xxx:
-	git submodule foreach --recursive "${MAKE} git.sprintcommit COMMIT_TIME=${COMMIT_TIME}"
+	git submodule foreach --recursive "${MAKE} git.sprintcommit COMMIT_TIME=0"
