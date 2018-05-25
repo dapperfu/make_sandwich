42aa9bed0bf8a3744df957dd57842bca41efbf6e
root <root+.mk_inc@n5010-LINUX>
2018-05-25 13:03:00 -0400

    Consolidated requirements files

--- env_host.mk
+++ env_host.mk
@@ -5,7 +5,7 @@
 
 # env.host - Setup the host environment.
 #
-# Requires admin permissions. 
+# Requires admin permissions.
 # Sets up the host machine with required packages
 .PHONY: env.host
 env.host: .env-${HOST}
@@ -14,16 +14,13 @@ env.host: .env-${HOST}
 .env-${HOST}: ${OSNAME}
 	$(file >${@},)
 
-.PHONY: LINUX 
+.PHONY: LINUX
 LINUX: ${LSB_ID}
-	
-.PHONY: Ubuntu
-Ubuntu: requirements-${LSB_ID}.txt
-	grep ${LSB_RELEASE} requirements-${LSB_ID}.txt | cut -f1 -d# | xargs apt-get install -y
 
-requirements-${LSB_ID}.txt:
-	$(file >${@},)
-	
+.PHONY: Ubuntu
+Ubuntu: requirements.txt
+	grep ${LSB_RELEASE} requirements.txt | cut -f2 -d":" | xargs apt-get install -y
+
 .PHONY: debug.host
 debug.host:
 	@$(info $${LSB_RELEASE}=${LSB_RELEASE})
