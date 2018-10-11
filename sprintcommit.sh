a54a56ce0fd047c86759d0fff28d9c3671fa2d8d
jed <jed+.mk_inc@m6700-LINUX>
2018-10-11 12:43:39 -0400

    jed@m6700: Thu Oct 11 16:43:07 UTC 2018

--- sprintcommit.sh
+++ sprintcommit.sh
@@ -10,21 +10,18 @@ do
 # Fetch.
 echo
 echo --- Sleeping until `date -d "+${COMMIT_TIME} second"` ---
-echo 
-git fetch --all --tags --prune --verbose
+echo
 
 # Commit.
 COMMIT_MSG=${COMMIT_MSG:-"`whoami`@`hostname`: `date --universal`"}
 echo
 echo --- Committing ${COMMIT_MSG}  ---
 echo
-git commit --all --message "${COMMIT_MSG}"
 
 # Push.
 echo
 echo --- Pushing ---
-echo 
-git push --all --verbose
+echo
 
 # For recursion.
 if [ "${COMMIT_TIME}" -eq "0" ]; then
@@ -33,6 +30,6 @@ fi
 # Sleep.
 echo
 echo --- Sleeping until `date -d "+${COMMIT_TIME} second"` ---
-echo 
-sleep ${COMMIT_TIME}
+echo
+sleep 10 # ${COMMIT_TIME}
 done
